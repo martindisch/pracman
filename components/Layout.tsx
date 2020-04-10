e6747aa22bb62076de9afb9d43e0836a273863a6
Martin Disch <martindisch@gmail.com>
2020-04-10 20:20:11 +0200

    Replace favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,7 +12,31 @@ const Layout = (props: Props) => (
   <>
     <Head>
       <title>{props.title}</title>
-      <link rel="icon" href="/favicon.ico" />
+
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#6200ee" />
+      <meta name="apple-mobile-web-app-title" content="Pracman" />
+      <meta name="application-name" content="Pracman" />
+      <meta name="msapplication-TileColor" content="#6200ee" />
+      <meta name="theme-color" content="#6200ee" />
+
       <link
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
         rel="stylesheet"
