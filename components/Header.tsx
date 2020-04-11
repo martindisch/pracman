15b0520ee4b380d93e01a8af47a0fdc861c66e54
Martin Disch <martindisch@gmail.com>
2020-04-11 09:49:55 +0200

    Add menu entry for login action

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import {
   TopAppBarActionItem,
   TopAppBarFixedAdjust,
@@ -7,28 +8,40 @@ import {
   TopAppBarSection,
   TopAppBarTitle,
 } from '@rmwc/top-app-bar';
+import { SimpleMenu, MenuItem, MenuSurfaceAnchor } from '@rmwc/menu';
 import styles from './Header.module.css';
 
-const Header = () => (
-  <>
-    <TopAppBar>
-      <TopAppBarRow>
-        <TopAppBarSection alignStart>
-          <Link href="/">
-            <a className={styles['sneaky-anchor']}>
-              <TopAppBarTitle>Pracman</TopAppBarTitle>
-            </a>
-          </Link>
-        </TopAppBarSection>
-        <TopAppBarSection alignEnd>
-          <Link href="/account">
-            <TopAppBarActionItem icon="account_circle" />
-          </Link>
-        </TopAppBarSection>
-      </TopAppBarRow>
-    </TopAppBar>
-    <TopAppBarFixedAdjust />
-  </>
-);
+const Header = () => {
+  const router = useRouter();
+  return (
+    <>
+      <TopAppBar>
+        <TopAppBarRow>
+          <TopAppBarSection alignStart>
+            <Link href="/">
+              <a className={styles['sneaky-anchor']}>
+                <TopAppBarTitle>Pracman</TopAppBarTitle>
+              </a>
+            </Link>
+          </TopAppBarSection>
+          <TopAppBarSection alignEnd>
+            <MenuSurfaceAnchor>
+              <SimpleMenu
+                focusOnOpen={false}
+                anchorCorner="bottomStart"
+                handle={<TopAppBarActionItem icon="account_circle" />}
+              >
+                <MenuItem onClick={() => router.push('/account')}>
+                  Login
+                </MenuItem>
+              </SimpleMenu>
+            </MenuSurfaceAnchor>
+          </TopAppBarSection>
+        </TopAppBarRow>
+      </TopAppBar>
+      <TopAppBarFixedAdjust />
+    </>
+  );
+};
 
 export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,9 @@ import '@material/top-app-bar/dist/mdc.top-app-bar.css';
 import '@material/icon-button/dist/mdc.icon-button.css';
 import '@material/ripple/dist/mdc.ripple.css';
 import '@rmwc/icon/icon.css';
+import '@material/menu/dist/mdc.menu.css';
+import '@material/menu-surface/dist/mdc.menu-surface.css';
+import '@material/list/dist/mdc.list.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <Component {...pageProps} />
