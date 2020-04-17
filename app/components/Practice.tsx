bebed71633e66bd72a3543c8f75d864cdebaa119
Martin Disch <martindisch@gmail.com>
2020-04-17 21:40:00 +0200

    Display practice snippets

--- app/components/Practice.tsx
+++ app/components/Practice.tsx
@@ -2,6 +2,8 @@ import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
 import { PracticeDetail } from './__generated__/PracticeDetail';
 import styles from './Practice.module.css';
+import { Card, CardPrimaryAction } from '@rmwc/card';
+import PracticeSnippet from './PracticeSnippet';
 
 const PRACTICE_DETAIL_QUERY = gql`
   query PracticeDetail($id: ID!) {
@@ -28,10 +30,19 @@ const Practice = (props: Props) => {
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
+  if (!data?.practice) return null;
+  const { practice } = data;
 
   return (
-    <div>
-      This is practice {props.id} with title {data?.practice?.title}
+    <div className={styles.content}>
+      <Card className={styles.card}>
+        <CardPrimaryAction className={styles['card-content']}>
+          <PracticeSnippet text={practice.title} icon="title" />
+          <PracticeSnippet text={practice.date} icon="event" />
+          <PracticeSnippet text={practice.kind} icon="category" />
+          <PracticeSnippet text={practice.location} icon="place" />
+        </CardPrimaryAction>
+      </Card>
     </div>
   );
 };
